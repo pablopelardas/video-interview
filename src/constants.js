7ee41efe04d8e2ff7849a44ebb0515ecdd657466
pablo pelardas <pablo.pelardas@gmail.com>
2022-07-08 20:19:37 -0300

    feat: add multiple questions

--- src/constants.js
+++ src/constants.js
@@ -1,28 +1,28 @@
 export const questionsMock = [
     {
         id: 1,
-        message: '¿Cuál es tu video juego favorito de la infancia?',
+        message: '¿Qué nos podrías contar sobre ti?',
         isAnswered: false,
         answer: [],
         duration: 0,
     },
     {
         id: 2,
-        message: '¿Cuál es tu video juego favorito de la infancia?',
+        message: '¿Cuáles son tus metas?',
         isAnswered: false,
         answer: [],
         duration: 0,
     },
     {
         id: 3,
-        message: '¿Cuál es tu video juego favorito de la infancia?',
+        message: '¿Cuáles son tus fortalezas y debilidades?',
         isAnswered: false,
         answer: [],
         duration: 0,
     },
     {
         id: 4,
-        message: '¿Cuál es tu video juego favorito de la infancia?',
+        message: '¿Por qué quieres trabajar con nosotros?',
         isAnswered: false,
         answer: [],
         duration: 0,
